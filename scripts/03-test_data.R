56866d3586d9ed030e74086149300675cd2ffa8a
leoyliu <liuyuanyi1208@gmail.com>
2024-04-01 17:07:18 -0400

    Upload Paper Structure

---
+++ scripts/03-test_data.R
@@ -0,0 +1,15 @@
+#### Preamble ####
+# Purpose: Tests
+# Author: Yuanyi (Leo) Liu
+# Date: 31 March 2024
+# Contact: leoy.liu@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+
+#### Test data ####
