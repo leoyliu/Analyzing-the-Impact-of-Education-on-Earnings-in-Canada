50b154bb7a9f6d951d6399c68d3816f93968c187
leoyliu <liuyuanyi1208@gmail.com>
2024-04-18 22:46:32 -0400

    update model and paper

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -18,21 +18,6 @@ library(modelsummary)
 #### Read data ####
 analysis_data <- read.csv(file = "data/analysis_data/analysis_data.csv")
 
-plot1 = analysis_data |>
-  ggplot(aes(x = Education_numeric, y = Avg.hourly.wage.rate)) +
-  geom_point(alpha = 0.5) +
-  theme_classic() + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
-plot1 +
-  geom_smooth(
-    method = "lm",
-    se = TRUE,
-    color = "black",
-    linetype = "dashed",
-    formula = "y ~ x"
-  )
-
 
 ### Model data ####
 first_model <-
@@ -44,12 +29,6 @@ first_model <-
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
     seed = 853
-  )
-
-modelsummary(
-  list(
-    "Auto-scaling priors" = first_model
-  )
 )
 
 
